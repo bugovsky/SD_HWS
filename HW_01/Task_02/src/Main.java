d2d48a492b71d1d521581ae96e41c7230ffe1275
bugovsky <drzhukovskiy@edu.hse.ru>
2022-11-06 23:26:09 +0300

    reformatted code

--- HW_01/Task_02/src/Main.java
+++ HW_01/Task_02/src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         for (int i = 2; i < 101; ++i) {
             boolean isPrime = true;
-            for (int j = 2; j < ((int)Math.sqrt(i)) + 1; ++j) {
+            for (int j = 2; j < ((int) Math.sqrt(i)) + 1; ++j) {
                 if (i % j == 0) {
                     isPrime = false;
                     break;
